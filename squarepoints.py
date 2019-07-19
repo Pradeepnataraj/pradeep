5e9fdd2b465d051c1a002d6b07a502697a515fbe
Pradeepnataraj <42169771+Pradeepnataraj@users.noreply.github.com>
2019-07-19 11:47:37 +0530

    Update squarepoints.py

--- squarepoints.py
+++ squarepoints.py
@@ -4,8 +4,8 @@ x3,y3=input().split();
 x4,y4=input().split();
 if(x1==x2 and x3==x4 or x1==x3 and x2==x4):
     if(y1==y2 and y3==y4 or y1==y3 and y2==y4):
-        print("true");
+        print("yes");
     else:
-        print("false");
+        print("no");
 else:
-    print("false");
+    print("no");
